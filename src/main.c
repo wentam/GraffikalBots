787e06d0343a2db0c03a3580570858dd7cec2e8a
wentam <wentam42@gmail.com>
2015-06-03 01:40:34 -0400

    fix scan arc

--- src/main.c
+++ src/main.c
@@ -138,12 +138,13 @@ void update(float time_step) {
 
     arcs[i]->location_x = bots[i]->location_x;
     arcs[i]->location_y = bots[i]->location_y;
-
+    printf("scanner offset: %i\n", g->tanks[i]->scanner_offset);
     arcs[i]->angle =
-        (((float)g->tanks[i]->heading + g->tanks[i]->scanner_offset +
-          (float)g->tanks[i]->turret_offset) *
-         1.4) -
-        ((scan_arc * 1.4) / 2);
+        ((((float)g->tanks[i]->heading + g->tanks[i]->scanner_offset +
+           (float)g->tanks[i]->turret_offset) *
+          1.4) -
+         ((scan_arc * 1.4) / 2)) -
+        90 % 360;
     arcs[i]->rot_x = 0;
     arcs[i]->rot_y = 0;
     arcs[i]->rot_z = -1;
